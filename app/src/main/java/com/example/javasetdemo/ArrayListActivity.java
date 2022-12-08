90c9c73ff6bdd27fd8693e32d96eeb6b42ade211
282330332 <282330332@qq.com>
2022-12-08 20:36:34 +0800

    ArrayList操作方法

--- app/src/main/java/com/example/javasetdemo/ArrayListActivity.java
+++ app/src/main/java/com/example/javasetdemo/ArrayListActivity.java
@@ -9,13 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * ArrayList 相关使用方法
  */
 public class ArrayListActivity extends AppCompatActivity {
-
-    private List<String> mList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +37,12 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onAddAll(View v) {
-
-
+        List<String> tList = new ArrayList<>();
+        tList.add("test1");
+        tList.add("test2");
+        List<String> mList = new ArrayList<>();
+        mList.addAll(tList);
+        log("addAll(): " + mList);
     }
 
     /**
@@ -47,8 +50,11 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onClear(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        log("clear(): " + mList);
+        mList.clear();
+        log("clear(): " + mList);
     }
 
     /**
@@ -56,8 +62,10 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onContains(View v) {
-        mList.add("test1");
-
+        List<String> tList = new ArrayList<>();
+        tList.add("test1");
+        tList.add("test2");
+        log("contains(): " + tList.contains("test2"));
     }
 
     /**
@@ -65,8 +73,14 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onContainsAll(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        List<String> tList = new ArrayList<>();
+        tList.add("test1");
+        tList.add("test2");
+        log("containsAll(): " + mList.contains(tList));
     }
 
     /**
@@ -74,8 +88,10 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onIsEmpty(View v) {
+        List<String> mList = new ArrayList<>();
+        log("isEmpty(): " + mList.isEmpty());
         mList.add("test1");
-
+        log("isEmpty(): " + mList.isEmpty());
     }
 
     /**
@@ -83,8 +99,38 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onIterator(View v) {
-        mList.add("test1");
+        List<String> mList = new ArrayList<>();
+        //添加集合元素
+        mList.add("鸡");
+        mList.add("你");
+        mList.add("美");
 
+        //建立List迭代器(ListIterator)
+        ListIterator<String> lit = mList.listIterator();
+
+//        E next(); 返回迭代中的下一个元素
+//        boolean hasNext(); 判断是否有下一个元素,有则返回true,无则false
+        while(lit.hasNext()){
+            String ss = lit.next();
+            if (ss.equals("你")){
+                //void add(E e); 将指定的元素插入列表(注意是lit调用add方法哦,是往！集合!中添加)
+                lit.add("太");
+            }
+            log(ss); // 用迭代器内部输出是没有添加"太"元素的
+        }
+        log(mList + "");
+
+//        E previous(); 返回迭代器中的上一个元素
+//        boolean hasPrevious(); 判断是否有上一个元素,有则返回true,无则false
+        while (lit.hasPrevious()){//逆向索引(从后往前)
+            String sss = lit.previous();
+            if (sss.equals("鸡")){
+//                void remove();从列表中删除由 next()或 previous()返回的最后一个元素（可选操作）
+                lit.remove();
+            }
+            log(sss); // 用迭代器内部输出是没有删除"鸡"元素的
+        }
+        log(mList + "");
     }
 
     /**
@@ -92,8 +138,15 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onRemove(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        log("remove(): " + mList);
+        mList.remove(0);
+        log("remove(): " + mList);
+        mList.remove("test3");
+        log("remove(): " + mList);
     }
 
     /**
@@ -101,8 +154,16 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onRemoveAll(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        log("removeAll(): " + mList);
+        List<String> tList = new ArrayList<>();
+        tList.add("test1");
+        tList.add("test2");
+        mList.removeAll(tList);
+        log("removeAll(): " + mList);
     }
 
     /**
@@ -110,8 +171,18 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onRetainAll(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        List<String> tList = new ArrayList<>();
+        tList.add("test1");
+        tList.add("test2");
+        log("onRetainAll() mList: " + mList);
+        log("onRetainAll() tList: " + tList);
+        mList.retainAll(tList);
+        log("onRetainAll() mList1: " + mList);
+        log("onRetainAll() tList2: " + tList);
     }
 
     /**
@@ -119,8 +190,11 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onSize(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        log("size() mList size: " + mList.size());
     }
 
     /**
@@ -128,8 +202,18 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onToArray(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
+        mList.add("test2");
+        mList.add("test3");
 
+//        String[] ts = (String[])mList.toArray(); // 报错：ClassCastException: java.lang.Object[] cannot be cast to java.lang.String[]
+
+        String[] ts = new String[mList.size()];
+        mList.toArray(ts);
+        for(int i = 0; i < ts.length; i ++) {
+            log("ts[" + i + "]:" + ts[i]);
+        }
     }
 
     /**
@@ -137,17 +221,24 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onGet(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        log("get(0):" + mList.get(0));
+        log("get(1):" + mList.get(1));
     }
 
     /**
-     * index()
+     * indexOf()
      * @param v
      */
     public void onIndex(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        log("index(1):" + mList.indexOf(1));
     }
 
     /**
@@ -155,7 +246,11 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onLastIndexOf(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
+        mList.add("test2");
+        mList.add("test3");
+        log("lastIndexOf(0):" + mList.lastIndexOf(0));
 
     }
 
@@ -164,8 +259,13 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onSet(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        log("get(0):" + mList.get(0));
+        mList.set(0, "test0");
+        log("get(0):" + mList.get(0));
     }
 
     /**
@@ -173,8 +273,12 @@ public class ArrayListActivity extends AppCompatActivity {
      * @param v
      */
     public void onSubList(View v) {
+        List<String> mList = new ArrayList<>();
         mList.add("test1");
-
+        mList.add("test2");
+        mList.add("test3");
+        List<String> tList = mList.subList(0, 2);
+        log("subList():" + tList);
     }
 
 
